3a5309183c084dafedbfd8c55c0a56b39e750b75
unknown <benguiratoussama1998@gmail.com>
2023-12-23 21:01:56 +0100

    various changes

---
+++ app.js
@@ -0,0 +1,7 @@
+const notionalInput = document.getElementById("notional-report-input");
+const processButton = document.getElementById("process-button");
+const resultField = document.getElementById("result");
+
+processButton.addEventListener("click", function() {
+  
+})
